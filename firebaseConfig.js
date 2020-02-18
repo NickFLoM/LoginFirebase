b861419fba2e839ac373e7f2c9d8c3215a01fbf9
NickFLoM <nicklopes02@hotmail.com>
2020-02-18 00:41:16 -0300

    Login com Firebase

---
+++ firebaseConfig.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBTlx9218VJyGVU6Nc90kpgxsDiCrSLHXU",
+    authDomain: "projetopromo-47fab.firebaseapp.com",
+    databaseURL: "https://projetopromo-47fab.firebaseio.com",
+    projectId: "projetopromo-47fab",
+    storageBucket: "projetopromo-47fab.appspot.com",
+    messagingSenderId: "106160458957",
+    appId: "1:106160458957:web:02eff89b2ca708ece26d84",
+    measurementId: "G-E50SWGSDC2"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
